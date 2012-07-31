6c25851f32d5123aea33d558f4a1142c2db7e45c
Patrik Carlsson <patrikc@comhem.se>
2012-07-31 08:49:25 +0200

    Added files.

---
+++ FSScrape/FSScrape/Http/IHttpService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mittan.FS.Http
+{
+    public interface IHttpService
+    {
+        string Get(string url, string content);
+        string Post(string url, string content);
+    }
+}
